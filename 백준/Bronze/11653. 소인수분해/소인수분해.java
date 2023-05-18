a12026a188569a541ccb6334fcbe0fcc6c290bbc
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-05-18 17:39:26 +0900

    [Bronze I] Title: 소인수분해, Time: 128 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       
+       int num = Integer.parseInt(br.readLine());
+       
+       for(int i = 2; i <= Math.sqrt(num); i++) {
+    	   while(num % i == 0) {
+    		   System.out.println(i);
+    		   num /= i;
+    	   }
+       }
+       
+       if(num != 1) {
+    	   System.out.println(num);
+       }
+	
+	}
+}
