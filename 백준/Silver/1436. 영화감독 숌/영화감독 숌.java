1928eb3fa805442b59d0df41d5a7a8e8114eee84
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-06-02 16:37:15 +0900

    [Silver V] Title: 영화감독 숌, Time: 300 ms, Memory: 86512 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -4,9 +4,6 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-	public static boolean[][] arr;
-	public static int min = 64; // 8 * 8
-
 	public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	
