04ca674fab3d18fc9c109111746dd55f1c1f5be5
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-06-06 14:51:25 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1448 ms, Memory: 129256 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,31 +1,30 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	StringBuilder sb = new StringBuilder();
     	
     	int n = Integer.parseInt(br.readLine());
     	
-    	int[] arr = new int[n];
+    	ArrayList<Integer> list = new ArrayList<>();
     	
     	for(int i = 0; i < n; i++) {
-    		arr[i] = Integer.parseInt(br.readLine());
+    		list.add(Integer.parseInt(br.readLine()));
     	}
     	
-    	Arrays.sort(arr);
+    	Collections.sort(list);
     	
-    	for(int i = 0; i < n; i++) {
-    		bw.write(arr[i] + "\n");
+    	for(int result : list) {
+    		sb.append(result).append('\n');
     	}
     	
-    	bw.flush();
+    	System.out.println(sb);
     	
     }
 }
