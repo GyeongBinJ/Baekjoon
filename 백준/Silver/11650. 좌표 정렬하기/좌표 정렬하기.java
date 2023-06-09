7ef372050f4d454d1a42293db7c6175937063055
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-06-09 16:39:30 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 808 ms, Memory: 56252 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -10,7 +8,6 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     	
     	int n = Integer.parseInt(br.readLine());
     	int[][] arr = new int[n][2];
@@ -29,9 +26,10 @@ public class Main {
     		}
     	});
     	
+    	StringBuilder sb = new StringBuilder();
     	for(int i = 0; i < n; i++) {
-    		System.out.println(arr[i][0] + " " + arr[i][1]);
+    		sb.append(arr[i][0] + " " + arr[i][1]).append("\n");
     	}
-    	
+    	System.out.println(sb);
     }
 }
