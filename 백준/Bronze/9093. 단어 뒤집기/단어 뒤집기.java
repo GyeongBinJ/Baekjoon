fd6cf8564c909fd48cd929fc228b341f0d269194
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-06-15 18:15:41 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 536 ms, Memory: 53636 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringBuilder result = new StringBuilder();
+    	
+    	int t = Integer.parseInt(br.readLine());
+    	
+    	for(int i = 0; i < t; i++) {
+    		StringTokenizer st = new StringTokenizer(br.readLine());
+    		while(st.hasMoreTokens()) {
+    			StringBuilder sb = new StringBuilder(st.nextToken());
+    			result.append(sb.reverse() + " ");
+    		}
+    		result.append("\n");
+    	}
+    	System.out.println(result);
+    }
+}
