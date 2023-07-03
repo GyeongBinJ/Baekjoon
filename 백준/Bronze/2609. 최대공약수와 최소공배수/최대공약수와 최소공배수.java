373e52f218f6cc0c4542fa0e33d44114aec3bae8
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-07-03 17:01:52 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -21,11 +21,14 @@ public class Main {
 	}
 	
 	public static int gcd(int a, int b) {
-		if(b == 0) {
-			return a;
+
+		while(b != 0) {
+			int r = a % b;
+			
+			a = b;
+			b = r;
 		}
-		
-		return gcd(b, a % b);
+		return a;
 	}
 	
 	
