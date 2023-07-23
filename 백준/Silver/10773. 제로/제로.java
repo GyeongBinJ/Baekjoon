cc62a1abe3e29a05ee540c47aa993a6f4d098225
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-07-23 17:27:53 +0900

    [Silver IV] Title: 제로, Time: 268 ms, Memory: 23932 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	int k = Integer.parseInt(br.readLine());
+    	
+    	Stack<Integer> stack = new Stack<Integer>();
+    	
+    	for(int i = 0; i < k; i++) {
+    		int num = Integer.parseInt(br.readLine());
+    		
+    		if(num != 0) {
+    			stack.push(num);
+    		} else if(num == 0){
+    			stack.pop();
+    		}
+    	}
+    	
+    	int sum = 0;
+    	
+    	while(!stack.isEmpty()) {
+    		sum += stack.pop();
+    	}
+    	
+    	System.out.println(sum);
+    	
+	}
+}	
