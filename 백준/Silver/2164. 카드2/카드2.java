7e157c265f1987fc57575043c4ee4844c3ae7fd9
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-07-26 15:09:47 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 45664 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -14,24 +14,19 @@ public class Main {
     	
     	int n = Integer.parseInt(br.readLine());
     	
-    	for(int i = 0; i < n; i++) {
-    		queue.offer(i + 1);
+    	for(int i = 1; i <= n; i++) {
+    		queue.offer(i);
     	}
     	
-    	while(queue.size() != 1) {
+    	while(queue.size() > 1) {
     		queue.poll();
-    		
     		queue.offer(queue.poll());
     		
     	}
     	
-    	while(!queue.isEmpty()) {
-    		sb.append(queue.poll());
-    	}
-    	
+    	sb.append(queue.poll());
     	System.out.println(sb);
     	
 	}
 	
 }
-	
