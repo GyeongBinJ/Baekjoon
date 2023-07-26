7cd5a93406577f1af445e96fca4905c321b32b58
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-07-26 15:05:20 +0900

    [Silver IV] Title: 카드2, Time: 200 ms, Memory: 53468 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringBuilder sb = new StringBuilder();
+    	
+    	Queue<Integer> queue = new LinkedList<Integer>();
+    	
+    	int n = Integer.parseInt(br.readLine());
+    	
+    	for(int i = 0; i < n; i++) {
+    		queue.offer(i + 1);
+    	}
+    	
+    	while(queue.size() != 1) {
+    		// 큐 첫번째 값 제거
+    		queue.remove();
+    		
+    		int num = queue.peek();
+    		queue.poll();
+    		queue.offer(num);
+    		
+    	}
+    	
+    	while(!queue.isEmpty()) {
+    		sb.append(queue.poll());
+    	}
+    	
+    	System.out.println(sb);
+    	
+	}
+	
+}
+	
