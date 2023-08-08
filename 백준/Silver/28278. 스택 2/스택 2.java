e24974e579dda6ca4729d26a93d3e70bee273110
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-08-08 17:00:43 +0900

    [Silver IV] Title: 스택 2, Time: 920 ms, Memory: 216292 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static int[] stack;
+	public static int size = 0;
+	
+	public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringBuilder sb = new StringBuilder();
+    	
+    	int n = Integer.parseInt(br.readLine());
+    	stack = new int[n];
+    	
+    	while(n--> 0) {
+    		StringTokenizer st = new StringTokenizer(br.readLine());
+    		
+    		switch (st.nextToken()) {
+			case "1": push(Integer.parseInt(st.nextToken()));
+			break;
+			case "2": sb.append(pop()).append("\n");
+			break;
+			case "3": sb.append(size()).append("\n");
+			break;
+			case "4": sb.append(empty()).append("\n");
+			break;
+			case "5": sb.append(peek()).append("\n");
+			break;
+			}
+    	
+    	}
+    	
+    	System.out.println(sb);
+    	
+	}
+	
+	public static void push (int num) {
+		stack[size] = num;
+		size++;
+	}
+	
+	public static int pop() {
+		if(size == 0) {
+			return -1;
+		} else {
+			int num = stack[size - 1];
+			stack[size - 1] = 0;
+			size--;
+			return num;
+		}
+	}
+	
+	public static int size() {
+		return size;
+	}
+	
+	public static int empty() {
+		if(size == 0) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	
+	public static int peek() {
+		if(size == 0) {
+			return -1;
+		} else {
+			return stack[size - 1];
+		}
+	}
+}
+	
