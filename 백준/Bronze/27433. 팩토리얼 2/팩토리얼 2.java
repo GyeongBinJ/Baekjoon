f0816e3c2626b5f5f2c33d2ec148000e0c058f4a
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-08-21 13:13:12 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] arr = new int[21];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
+		}
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		long result = 1;
+		
+		for(int i = 0; i < num; i++) {
+			result *= arr[i];
+		}
+		
+		System.out.println(result);
+		
+	}
+	
+}
+	
