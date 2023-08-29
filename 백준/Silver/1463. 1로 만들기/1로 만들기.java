10a741d458b9988f31d4217b52ca1a1d483012bd
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-08-29 15:16:04 +0900

    [Silver III] Title: 1로 만들기, Time: 132 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -4,34 +4,20 @@ import java.io.InputStreamReader;
 
 public class Main {
 	
-	static Integer[] dp;
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
+		System.out.println(recur(n, 0));
 		
-		dp = new Integer[n + 1];
-		dp[0] = dp[1] = 0;
-		
-		System.out.println(recur(n));
 	}
 	
-	static int recur(int n) {
+	static int recur(int n, int count) {
 		
-		if(dp[n] == null) {
-			if(n % 6 == 0) {
-				dp[n] = Math.min(recur(n - 1), Math.min(recur(n / 3), recur(n / 2))) + 1;
-			} else if(n % 3 == 0) {
-				dp[n] = Math.min(recur(n / 3), recur(n - 1)) + 1;
-			} else if(n % 2 == 0) {
-				dp[n] = Math.min(recur(n / 2), recur(n - 1)) + 1;
-			} else {
-				dp[n] = recur(n - 1) + 1;
-			}
+		if(n < 2) {
+			return count;
 		}
 		
-		return dp[n];
+		return Math.min(recur(n / 2, count + 1 + (n % 2)), recur(n / 3, count + 1 + (n % 3)));
 	}
-
 }
