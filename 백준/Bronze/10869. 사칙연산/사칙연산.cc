f076b80aa9a52a1a3b2ea84927ee4a7c1bee9ed6
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-09-12 16:47:41 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.cc
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -5,12 +5,13 @@ using namespace std;
 int main()
 {
 	int a, b;
+	
 	cin >> a >> b;
 
-	cout << (a + b) << endl;
-	cout << (a - b) << endl;
-	cout << (a * b) << endl;
-	cout << (a / b) << endl;
-	cout << (a % b) << endl;
+	cout << a + b << "\n";
+	cout << a - b << "\n";
+	cout << a * b << "\n";
+	cout << a / b << "\n";
+	cout << a % b << "\n";
 	return 0;
 }
