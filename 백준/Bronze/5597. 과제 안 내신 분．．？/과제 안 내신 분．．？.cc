f5a8fe1ba813b6428ad458a46b9af7a934434261
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-10-16 14:53:25 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -6,27 +6,17 @@ int main()
 {
 	ios_base::sync_with_stdio(false);
 
-	int arr[30];
-	int arr2[28];
+	int arr[31] = { 0, };
+	int num;
 
 	for (int i = 0; i < 28; i++) {
-		cin >> arr2[i];
+		cin >> num;
+		arr[num] = 1;
 	}
 
-	for (int i = 0; i < 30; i++) {
-		arr[i] = i + 1;
-	}
-
-	for (int i = 0; i < 30; i++) {
-		for (int j = 0; j < 28; j++) {
-			if (arr[i] == arr2[j])
-				arr[i] = 0;
-		}
-	}
-
-	for (int i = 0; i < 30; i++) {
-		if (arr[i] != 0)
-			cout << arr[i] << "\n";
+	for (int i = 1; i <= 30; i++) {
+		if (!arr[i])
+			cout << i << "\n";
 	}
 
 	return 0;
