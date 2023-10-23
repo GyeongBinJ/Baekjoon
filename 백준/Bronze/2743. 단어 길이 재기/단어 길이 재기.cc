2a1bc271eb0afd0b22e6f0fecc7cb4a71e3fec2c
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-10-23 15:47:19 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(0);
+
+	string str;
+	cin >> str;
+	
+	cout << str.length();
+
+	return 0;
+}
