5e78cb8caed422f81059588dd165b3a7bd760016
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-10-30 15:59:20 +0900

    [Bronze II] Title: 단어의 개수, Time: 8 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(0);
+
+	string str;
+	getline(cin, str);
+
+	int count = 0;
+
+	if (str.empty()) {
+		cout << 0;
+		return 0;
+	}
+
+	count++;
+
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == ' ')
+			count++;
+	}
+
+	if (str[0] == ' ')
+		count--;
+	if (str[str.length() - 1] == ' ')
+		count--;
+
+	cout << count;
+	return 0;
+}
