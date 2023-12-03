7a9765537b6999e938c8e41855323020ccf8d265
GyeongBinJ <120453200+GyeongBinJ@users.noreply.github.com>
2023-12-03 20:01:16 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+	int angle1, angle2, angle3;
+
+	cin >> angle1 >> angle2 >> angle3;
+
+	if (angle1 == 60 && angle2 == 60 && angle3 == 60) {
+		cout << "Equilateral";
+	}
+	else if (angle1 + angle2 + angle3 == 180) {
+		if (angle1 == angle2 || angle1 == angle3 || angle2 == angle3) {
+			cout << "Isosceles";
+		}
+		else {
+			cout << "Scalene";
+		}
+	}
+	else {
+		cout << "Error";
+	}
+	
+	return 0;
+}
